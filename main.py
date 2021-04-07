5f4606481d3aa4f6378d9b0901532a3c72285e05
Denis <gostylo_d@europe.com>
2021-04-07 22:48:37 +0300

    Added "Position Logger" mode

--- main.py
+++ main.py
@@ -1,26 +1,40 @@
 import pyautogui
 import sys
+import time
 from pynput import keyboard
 
 exec_stop = False
+mode = "autoclicker"
 
 
 def run():
     while exec_stop is False:
-        try:
-            pyautogui.moveTo(951, 946, 0.5)
-            pyautogui.click(951, 946)
-            pyautogui.moveTo(967, 656, 0.5)
-            pyautogui.click(967, 656)
-            pyautogui.moveTo(533, 863, 0.5)
-            pyautogui.click(533, 863)
-        except:
-            print("An error occurred: ", sys.exc_info()[0])
+        if mode == "autoclicker":
+            try:
+                pyautogui.moveTo(951, 946, 0.5)
+                pyautogui.click(951, 946)
+                pyautogui.moveTo(967, 656, 0.5)
+                pyautogui.click(967, 656)
+                pyautogui.moveTo(533, 863, 0.5)
+                pyautogui.click(533, 863)
+            except:
+                print("An error occurred: ", sys.exc_info()[0])
+        else:
+            try:
+                print(pyautogui.position())
+                time.sleep(1)
+            except:
+                print("An error occurred: ", sys.exc_info()[0])
     sys.exit(0)
 
 
 def on_press(key):
-    if key == keyboard.Key.f12:
+    if key == keyboard.Key.f11:
+        global mode
+        mode = "pos_logger" if mode == "autoclicker" else "autoclicker"
+        print("Toggling mode...")
+        return
+    elif key == keyboard.Key.f12:
         print("Shutting down...")
         global exec_stop
         exec_stop = True
